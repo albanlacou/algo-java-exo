33d5d191848b6ffc322a392bc7a10350c7504982
Admin <lacou.alban@gmail.com>
2020-10-05 16:25:28 +0200

    exos fonction

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,21 +1,62 @@
 package com.company;
 
+
+
+
+
+
+
 public class Main {
+
+    static void printScores(int tableau[]){
+
+        for(int i = 0;i<5;i++){
+
+            System.out.println(tableau[i]);
+
+        }
+    }
+
+    static int BestScore(int tableau[]){
+        int max = -10;
+
+        for(int i = 0;i<5;i++){
+             max = 0;
+
+            if (tableau[i]>max){
+                max = tableau[i];
+
+            }
+
+        }return max;
+    }
+
+    static boolean MoinsDe10(int tableau[]){
+        boolean isMoinsDe10 = false;
+        for(int i=0;i<tableau.length;i++){
+           if (tableau[i]<10){
+               isMoinsDe10 = true;
+               return isMoinsDe10;
+           }
+
+        }return false;
+    }
+
+
+
 
     public static void main(String[] args) {
 	int[] score = new int[5];
-	    score[0] = 23;
+	    score[0] = 74;
         score[1] =75 ;
         score[2] =25 ;
         score[3] =45 ;
         score[4] =76 ;
 
-        for(int i = 0;i<5;i++){
-            System.out.println(score[i]);
 
-        }
         float moyenne = ((score[1]+score[2]+score[3]+score[4]+score[0])/5);
-        System.out.println("La moyenne de vos scores est:"+moyenne);
+        boolean moinsDe10 = MoinsDe10(score);
+        System.out.println(moinsDe10);
 
     }
 
